29d9ffc20c8ea979951e2b0bafda43c6716497f2
JHONNY <jhonny_luiz_c@hotmail.com>
2018-11-23 10:20:18 -0200

    Correção de delete

--- maxpedidos-app/src/app/modules/produtos/table-produtos/table-produtos.component.ts
+++ maxpedidos-app/src/app/modules/produtos/table-produtos/table-produtos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from '../produto';
 import { ProdutoService } from '../produto.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-table-produtos',
@@ -10,13 +11,23 @@ import { ProdutoService } from '../produto.service';
 export class TableProdutosComponent implements OnInit {
 
   produtos: Produto[];
-  displayedColumns: string[] = ['id', 'descricao', 'preco','acoes'];
+  displayedColumns: string[] = ['id', 'descricao', 'preco', 'acoes'];
+  displayTable: boolean;
 
-  constructor(private produtoService: ProdutoService) { }
+  constructor(private produtoService: ProdutoService, private router: Router) { }
 
   ngOnInit() {
     this.produtoService.getTodos().subscribe(res => {
       this.produtos = res.data;
     });
+  }
+
+  apagarProduto(id: number, index: number) {
+    this.produtoService.apagarProduto(id).subscribe(res => {
+      this.produtos = this.produtos.splice(index, 1);
+      if (this.produtos.length == 0)
+        console.log(this.produtos.length)
+      }
+    );
   }
 }
